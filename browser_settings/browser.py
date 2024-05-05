266f5bec2f088c6b091473d9ffd7c17d1d810896
axelcarapinha <axelcarapinha@gmail.com>
2024-05-05 21:12:03 +0100

    Changed firefox code again - will test on VM

--- browser_settings/browser.py
+++ browser_settings/browser.py
@@ -3,22 +3,31 @@ from selenium.webdriver.firefox.options import Options
 
 SYSTEM_PROXY = 5  # number checked on about:config firefox "page"
 
+class MyCustomError(Exception):
+	pass
+
 def set_proxy_with_option(options, profile, opt_number):	
+	if not opt_number:
+		raise MyCustomError("opt number is null. Please, use a valid value.")
 	
 	profile.set_preference("network.proxy.type", opt_number)
 	profile.set_preference("network.proxy.no_proxies_on", "localhost, 127.0.0.1")
-	
 	options.profile = profile # Use the options when launching Firefox
 
 # Define the user instances
 options = Options()
 profile = webdriver.FirefoxProfile()
 
-options.headless = True	
 set_proxy_with_option(options, profile, SYSTEM_PROXY)	
 
+# Permit the usage in non-graphical environment
+options.headless = True
+
 # Apply and quit
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox(options=options) 
 
 print("Firefox proxy settings successfully changed.")
 driver.quit()
+
+
+
