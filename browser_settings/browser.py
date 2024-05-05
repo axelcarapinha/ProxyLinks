1467a2227d0437921c227ca67e5a8d3d41d7759d
axelcarapinha <axelcarapinha@gmail.com>
2024-05-05 22:15:09 +0100

    Use of firefox profile from other perspective

--- browser_settings/browser.py
+++ browser_settings/browser.py
@@ -3,12 +3,12 @@ from selenium.webdriver.firefox.options import Options
 
 SYSTEM_PROXY = 5  # number checked on about:config firefox "page"
 
-class MyCustomError(Exception):
+class InvalidProxyOption(Exception):
 	pass
 
 def set_proxy_with_option(options, profile, opt_number):	
 	if not opt_number:
-		raise MyCustomError("opt number is null. Please, use a valid value.")
+		raise InvalidProxyOption("opt number is null. Please, use a valid value.")
 	
 	profile.set_preference("network.proxy.type", opt_number)
 	profile.set_preference("network.proxy.no_proxies_on", "localhost, 127.0.0.1")
@@ -16,7 +16,7 @@ def set_proxy_with_option(options, profile, opt_number):
 
 # Define the user instances
 options = Options()
-profile = webdriver.FirefoxProfile()
+profile = webdriver.FirefoxProfile() 
 
 set_proxy_with_option(options, profile, SYSTEM_PROXY)	
 
@@ -26,8 +26,7 @@ options.headless = True
 # Apply and quit
 driver = webdriver.Firefox(options=options) 
 
-print("Firefox proxy settings successfully changed.")
-driver.quit()
+print("Firefox profile with proxy enabled created.")
 
 
 
